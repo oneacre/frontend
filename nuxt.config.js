105216952c64cb8964fca1cc9f39f09b5c9312a5
Stef Kors <stef.kors@gmail.com>
2021-04-26 19:05:39 +0200

    update api url

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ import { markdownCustomCompiler } from './helpers/markdownCustomCompiler'
 const url =
   process.env.NODE_ENV === 'development'
     ? process.env.API || 'http://localhost:8080'
-    : 'https://api.oneacre.online'
+    : 'http://api.oneacre.online'
 // TODO: Should this link to a defualt product / test env?
 console.log(url)
 
